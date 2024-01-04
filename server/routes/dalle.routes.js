0ee522989e4d4ebb0f4017020dab81191e775ee6
KaranGulve4342 <karangulve4342@gmail.com>
2024-01-04 11:40:30 +0530

    commited configuration error

--- server/routes/dalle.routes.js
+++ server/routes/dalle.routes.js
@@ -1,16 +1,21 @@
 import express from 'express';
 import * as dotenv from 'dotenv';
-import { Configuration, OpenAIApi} from 'openai';
+// import { Configuration, OpenAIApi} from 'openai';
+import OpenAI from 'openai';
 
 dotenv.config();
 
 const router = express.Router();
 
-const config = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
-});
+// const config = new Configuration({
+//   apiKey: process.env.OPENAI_API_KEY,
+// });
 
-const openai = new OpenAIApi(config);
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+})
+
+// const openai = new OpenAIApi(config);
 
 router.route('/').get((req, res) => {
   res.status(200).json({ message: "Hello from DALL.E ROUTES" })
